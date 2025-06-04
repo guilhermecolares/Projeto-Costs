79747e0ecb788921484fbd60825b67eb9bf324c7
guilhermecolares <guilhermegraf17@gmail.com>
2025-06-04 15:15:27 -0300

    CRIAÇÃO DE API COM JSON SERVER, E CONECTANDO COM REACT

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -9,6 +9,10 @@ function Select({ text, name, options, handleOnChange, value }) {
 
             <select name={name} id={name}>
                 <option>Selecione uma opção</option>
+                {options.map((option) => (
+                    <option 
+                    value={option.id} key={option.id}>{option.name}</option>
+                ))}
             </select>
         </div>
     )
--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,9 +1,26 @@
+import { useEffect, useState } from 'react'
+
 import styles from './ProjectForm.module.css'
 import Input from '../form/Input'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
 
 function ProjectForm({btnText}) {
+
+    const [categorias, setCategorias] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:5000/categorias', {
+        method: 'get',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+        })
+        .then((response) => response.json())
+        .then((data => setCategorias(data)))
+        .catch((err) => console.log(err))
+    }, [])
+
     return (
         <form className={styles.form}>
             <Input 
@@ -23,6 +40,7 @@ function ProjectForm({btnText}) {
             <Select 
             name='category_id'
             text='Selecione uma categoria'
+            options={categorias}
             />
 
             <SubmitButton 
