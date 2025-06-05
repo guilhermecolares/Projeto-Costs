cb310d43bf52e75fbb259c0fdaa501b7d20e700a
guilhermecolares <guilhermegraf17@gmail.com>
2025-06-05 18:59:13 -0300

    CRIANDO A PARTE VISUAL DE PROJECT

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,6 +1,10 @@
 import { useLocation } from "react-router-dom"
 
 import Message from "../layout/Message"
+import Container from "../layout/Container"
+import LinkButton from "../layout/LinkButton"
+
+import styles from "./styles/Projects.module.css"
 
 function Projects() {
 
@@ -11,11 +15,19 @@ function Projects() {
     }
 
     return (
-        <div>
-            <h1>Meus Projetos</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to='/newproject' text='Criar Projeto' />
+            </div>
+
             {msg && 
                 <Message text={msg} type='success' />
             }
+
+            <Container customClass='start'>
+                <p>Projetos...</p>
+            </Container>
         </div>
     )
 }
